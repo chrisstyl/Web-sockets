70bfa49a1bbedbb8305a3293a4e62efa54e2ccd1
Unknown <christosstylianou99@gmail.com>
2019-11-01 00:02:21 +0000

    more functions added
    recv,sends,put done

--- lab3.py
+++ lab3.py
@@ -1,6 +1,11 @@
 import sys
 import socket
 
+def open_file(filename)
+	with open(filename, mode="rb") as file:
+		file_bytes = file.read()
+		file_size=len(file)
+		return file_bytes,file_size
 def send_file(socket, filename):
 	with open(filename, mode="rb") as file:
 		file_bytes = file.read(4096)
@@ -8,20 +13,8 @@ def send_file(socket, filename):
 			socket.send(file_bytes)
 			file_bytes = file.read(4096)
 
-def recv_file(socket, filename):
-	with open(filename, mode='wb') as file:
-		data = socket.recv(4096)
-		while data:
-			file.write(data)
-			data = socket.recv(4096)
 
 def get_header(socket, sock_addr):
-	"""Reads data from a passed socket and prints it on screen.
-
-	Returns either when a newline character is found in the stream or the connection is closed.
-        The return value is the total number of bytes received through the socket.
-	The second argument is prepended to the printed data to indicate the sender.
-	"""
 	print(sock_addr + ": ", end="", flush=True) # Use end="" to avoid adding a newline after the communicating partner's info, flush=True to force-print the info
 
 	data = bytearray(1)
@@ -32,6 +25,10 @@ def get_header(socket, sock_addr):
 	 other side was processed, as the assumption is that the client will send one line at
 	 a time).
 	"""
+	header=str(request)+"\0"+str(filename)+"\0"+str(size)
+	header_size=len(bytes(header))
+	socket.sendall(header_size)
+	socket.sendall(bytes(header))
 	data = socket.recv(4096)
 	if len(data) > 0 and "\n" not in data.decode():
 		"""
@@ -44,12 +41,13 @@ def get_header(socket, sock_addr):
 		return data
 	else return 0
 def socket_to_screen(socket, sock_addr):
-	"""Reads data from a passed socket and prints it on screen.
+#
+	# Reads data from a passed socket and prints it on screen.
 
-	Returns either when a newline character is found in the stream or the connection is closed.
-        The return value is the total number of bytes received through the socket.
-	The second argument is prepended to the printed data to indicate the sender.
-	"""
+	# Returns either when a newline character is found in the stream or the connection is closed.
+    #     The return value is the total number of bytes received through the socket.
+	# The second argument is prepended to the printed data to indicate the sender.
+	# """
 	print(sock_addr + ": ", end="", flush=True) # Use end="" to avoid adding a newline after the communicating partner's info, flush=True to force-print the info
 
 	data = bytearray(1)
@@ -88,5 +86,70 @@ def keyboard_to_socket(socket):
 		return 0
 
 	# Send the whole line through the socket; remember, TCP provides no guarantee that it will be delivered in one go.
-	bytes_sent = socket.sendall(str.encode(user_input))
+	#bytes_sent = socket.sendall(str.encode(user_input))
 	return bytes_sent
+
+
+def recv_all(size):
+	msg=0
+	while 0<size:
+		if size>4096:
+			size-=4096
+			msg+=socket.recv(4096)
+		else:
+			msg+=socket.recv(size)
+			size=0
+	return size
+
+def get_header_size(header):
+	header_size=len(bytes(header))
+	return header_size
+def send_header_size(cli_sock,header_size):
+	return cli_sock.sendall(header_size)
+
+def recv_header_size():
+	header_size=int(recv(24),2)
+	return header_size
+def send_header(command,filename,size):
+	header=str(request)+"\0"+str(filename)+"\0"+str(size)
+	get_header_size(header)
+	socket.
+def existingfile(filename):
+	try:
+		f = open(filename)
+		f.close()
+	except FileNotFoundError:
+		print('File does not exist')
+		return False
+	return FileExistsError("Cannot create file as file already exists")
+def put_send(cli_sock,filename):
+	file,file_size=open_file(filename)
+	header="put"+"\0"+"filename"+"\0"+"file_size" #+"\0"
+	header_size=get_header_size(header)
+	print("Errors while sending header size:",send_header_size(header_size))
+	print("Errors while sending file:",cli_sock.sendall(file))
+
+def recv_start(cli_sock):
+	commandsdict={"put":recv_put(),
+			"get":recv_get(),
+			"list":recv_list()}
+	header_size=recv_header_size()
+	header=recv(header_size)
+	header=header.split("\0")
+	if header[0] in commandsdict:
+		commandsdict[header[0]]
+	else raise SyntaxError("No such command found")
+	
+	filename=header
+	with open(filename,mode="xb") as f:
+
+
+def recv_file(socket, filename):
+	with open(filename, mode='xb') as file:
+		data = socket.recv(4096)
+		while data:
+			file.write(data)
+			data = socket.recv(4096)
+
+
+	
